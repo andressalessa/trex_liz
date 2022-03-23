71cd23599378d71a9a7f80c850ea60842552a981
andressalessa <lessadasilveiraandressa@gmail.com>
2022-03-23 19:55:49 -0300

    primeira aula demonstrativa

--- sketch.js
+++ sketch.js
@@ -3,12 +3,21 @@ var box;
 function setup() {
   createCanvas(400, 400);
 
-  box = createSprite(200,200);
+  box = createSprite(200, 200, 50, 50);
   box.shapeColor = "red";
+
 }
 
 function draw() {
-  background(220);
+  background("black");
 
+  if (keyIsDown(UP_ARROW)) {
+    box.y -= 3;
+  }
+
+  if (keyIsDown(DOWN_ARROW)) {
+    box.y += 3;
+  }
+  
   drawSprites();
 }
