52cad5fce9f47a442d9f38fc355a8832e5f8082f
andressalessa <lessadasilveiraandressa@gmail.com>
2022-03-30 19:24:05 -0300

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,23 +1,40 @@
-var box;
+// Variável Global
+var trex ,trex_running;
+var edges;
+var ground;
 
-function setup() {
-  createCanvas(400, 400);
-
-  box = createSprite(200, 200, 50, 50);
-  box.shapeColor = "red";
-
+function preload(){
+  trex_running = loadAnimation("trex1.png", "trex3.png", "trex4.png");
+  
 }
 
-function draw() {
-  background("black");
-
-  if (keyIsDown(UP_ARROW)) {
-    box.y -= 3;
-  }
-
-  if (keyIsDown(DOWN_ARROW)) {
-    box.y += 3;
-  }
+function setup(){
+  createCanvas(600,200)
   
+  //crie um sprite de trex
+  trex = createSprite(50,160,20,50);
+  trex.addAnimation("running", trex_running);
+  trex.scale = 0.5;
+  // trex.x = 50;
+  
+  edges = createEdgeSprites();
+
+  ground = createSprite(200, 180, 400, 20);
+  
+}
+
+function draw(){
+  background("white");
+
+  
+  if (keyDown('space')) {
+    trex.velocityY = -10;
+  }
+
+  trex.velocityY = trex.velocityY + 0.5;
+  
+  trex.collide(ground);
+
   drawSprites();
+
 }
